cb3242ed3f10fd53d3ce8a6409457c2f2b5e155f
liujiamin <2269245204@qq.com>
2017-05-25 18:14:19 +0800

    为HomeActivity添加ToolBar

--- app/src/main/java/com/konka/dailynews/ui/activity/HomeActivity.java
+++ app/src/main/java/com/konka/dailynews/ui/activity/HomeActivity.java
@@ -2,6 +2,7 @@ package com.konka.dailynews.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.konka.dailynews.R;
 import com.konka.dailynews.base.BaseActivity;
@@ -32,6 +33,14 @@ public class HomeActivity extends BaseActivity
     @Override
     public void initToolBar()
     {
+        toolbar.setTitle("头条");
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_home,menu);
+        return true;
     }
 }
