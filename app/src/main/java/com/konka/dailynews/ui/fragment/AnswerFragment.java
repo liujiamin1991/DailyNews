c6c115c5526a6f2da4440c71908d6f3cf4165743
liujiamin <2269245204@qq.com>
2017-06-01 18:38:43 +0800

    添加加载时旋转图标

--- app/src/main/java/com/konka/dailynews/ui/fragment/AnswerFragment.java
+++ app/src/main/java/com/konka/dailynews/ui/fragment/AnswerFragment.java
@@ -1,13 +1,25 @@
 package com.konka.dailynews.ui.fragment;
 
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import com.konka.dailynews.R;
 import com.konka.dailynews.base.BaseFragment;
+
+import butterknife.Bind;
 
 /**
  * Created by ljm on 2017-5-27.
  */
 public class AnswerFragment extends BaseFragment
 {
+    @Bind(R.id.iv_progress)
+    ImageView ivProgress;
+
+    private Animation animation;
+
     public AnswerFragment()
     {
         super();
@@ -22,6 +34,26 @@ public class AnswerFragment extends BaseFragment
     @Override
     protected void initView()
     {
-        
+        initProgressAnim();
+    }
+
+    private void initProgressAnim()
+    {
+        animation = AnimationUtils.loadAnimation(activity,R.anim.progress_rotate);
+        animation.setFillAfter(true);
+    }
+
+    private void showProgress()
+    {
+        if(animation == null) return;
+
+        ivProgress.setVisibility(View.VISIBLE);
+        ivProgress.startAnimation(animation);
+    }
+
+    private void hideProgress()
+    {
+        ivProgress.clearAnimation();
+        ivProgress.setVisibility(View.INVISIBLE);
     }
 }
